0fd29b8c74e720c727d0083b451372b740b35ee3
rory.martin <rory.martin08@outlook.com>
2025-09-05 13:13:34 +0100

    made some repositories and html file

---
+++ may_26/html_basics/version_1/index.php
@@ -0,0 +1,31 @@
+ <?php //this opens the php code section
+
+ echo "<!DOCTYPE HTML>";
+
+ echo "<html>"; //pushes through the html code to initialise the code
+ echo "<head>"; //
+ echo "<title>Version 1</title>";
+
+ echo "</head>";
+ echo "<body>";
+ echo "<a href='page2.php'>Page 2</a>'>";
+ echo "<img src='FontainesDC_Romance_4000x40002.jpg'>";
+ echo "<table>";
+  echo "<tr>";
+     echo "<td>hello</td>";
+  echo "</tr>";
+ echo "<h1>Version 1</h1>";
+ echo "<p>This is a version 1.</p>";
+ echo "<hr>";
+ echo "<br>";
+ echo "<em>Version 1</em>";
+ echo "<i>Version 1</i>";
+ echo "<strong>Version 1</strong>";
+ echo "<b>Version 1</b>";
+ echo "</body>";
+
+ echo "</html>";
+
+ ?>
+
+
