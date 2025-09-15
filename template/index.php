c2a22a492da94ec8829e8d52464b0543ec04ae21
rory.martin <rory.martin08@outlook.com>
2025-09-15 12:16:17 +0100

    templates

---
+++ template/index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+echo "<!DOCTYPE HTML>";
+
+echo "<html>";
+echo "<head>";
+
+echo "<link rel='stylesheet' href='css/styles.css' />";  # links to the external style sheet
+
+echo "</head>";
+echo "<body>";
+
+echo "<div class='container'>";
+require_once "assets/topbar.php";
+require_once "assets/nav.php";
+
+echo "<div class='content'>";
+
+
+echo "</div>";
+echo "</div>";
+
+?>
