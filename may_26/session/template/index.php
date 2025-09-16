9fa0732f327921cd49a6e902be28c8bbe031a3a9
rory.martin <rory.martin08@outlook.com>
2025-09-16 14:49:02 +0100

    error

--- may_26/session/template/index.php
+++ may_26/session/template/index.php
@@ -27,4 +27,14 @@ echo "<input type='Submit' value='Submit' />";
 echo "</div>";
 echo "</div>";
 
+
+// Remove all illegal characters from a url
+$url = filter_var($url, FILTER_SANITIZE_URL);
+
+// Validate url
+if (!filter_var($url, FILTER_VALIDATE_URL) === false) {
+    echo("$url is a valid URL");
+} else {
+    echo("$url is not a valid URL");
+}
 ?>
