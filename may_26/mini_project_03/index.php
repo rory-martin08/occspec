c786368ddd65df45a3ab9447f08e24726e93fc73
rory.martin <rory.martin08@outlook.com>
2025-09-18 13:14:46 +0100

    form

--- may_26/mini_project_03/index.php
+++ may_26/mini_project_03/index.php
@@ -2,7 +2,9 @@
 
 <?php
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
+}
 echo "<!DOCTYPE HTML>";
 
 echo "<html>";
@@ -18,6 +20,21 @@ require_once "assets/topbar.php";
 require_once "assets/nav.php";
 
 echo "<div class='content'>";
+
+echo "<form method='post' action=''>";
+echo "<input type='Text' name='Name' placeholder='Name' />";
+echo "<input type='password' name='password' placeholder='password' />";
+echo "<input type='Submit' value='Submit' />";
+echo "</div>";
+echo "</div>";
+
+$pattern = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/';
+
+if (preg_match($pattern, $password)) {
+    echo "Valid Password";
+} else {
+    echo "Invalid Password";
+}
 
 
 echo "</div>";
